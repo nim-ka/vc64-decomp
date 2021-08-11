84f910f79f150bf83538e1af03f799db38cf2c62
onlymx13 <onlymx13@gmail.com>
2021-08-10 21:11:40 -0500

    Working and better diff script

--- diff_settings.py
+++ diff_settings.py
@@ -5,3 +5,5 @@ def apply(config, args):
     config['myimg'] = 'build/vc64_00000001.app'
     config['mapfile'] = 'build/vc64.map'
     config['source_directories'] = ['src', 'asm']
+    config['arch'] = 'ppc'
+    config["objdump_executable"] = "$DEVKITPPC/bin/powerpc-eabi-objdump"
